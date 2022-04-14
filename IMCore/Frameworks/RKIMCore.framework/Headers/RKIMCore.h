586e570cd334dad76ed1c9b6035c1abbe8c04f75
chzy <yang.chunzhi@hotmail.com>
2022-04-14 20:50:34 +0800

    [feat] gitIgnore修改

---
+++ IMCore/Frameworks/RKIMCore.framework/Headers/RKIMCore.h
@@ -0,0 +1,18 @@
+//
+//  RKIMCore.h
+//  RKIMCore
+//
+//  Created by chzy on 2022/4/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RKIMCore.
+FOUNDATION_EXPORT double RKIMCoreVersionNumber;
+
+//! Project version string for RKIMCore.
+FOUNDATION_EXPORT const unsigned char RKIMCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKIMCore/PublicHeader.h>
+
+
